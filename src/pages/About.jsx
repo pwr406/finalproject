68a2eb7e985fb2df19f533956ac9dd0b42f716b7
Paul Ryan <paul.walker.ryan@gmail.com>
2023-10-13 00:56:25 -0600

    css hell

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,13 +2,15 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
+import '../App.css'
 
 export default function About () {
     return (
         <Container fluid>
-      <Row>
-        <Col><Image src="../assets/missoula.jpg" fluid /></Col>
+      <Row className="imageRow">
+        <Col><Image src="../assets/missoula.jpg" fluid className="backgroundImage" /></Col>
       </Row>
+     
     </Container>
     )
 }
