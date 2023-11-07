d9642cfc2286b43d80bedb2194cc6e0cf06f137f
Paul Ryan <paul.walker.ryan@gmail.com>
2023-11-06 21:57:54 -0700

    trying again

--- api/getData.js
+++ api/getData.js
@@ -1,18 +1,13 @@
-
-export default async function handler(req, res) {
-  const url = 'https://raw.githubusercontent.com/pwr406/finalproject/main/db.json?token=GHSAT0AAAAAACI65HSHP2TNWK72QAPCB5BMZKJXTAA';
-
-  try {
-    const response = await fetch(url);
-    const rawData = await response.text(); // Fetch as text
-
-    // Manually parse the fetched text as JSON
-    const data = JSON.parse(rawData);
-
-    // Return the data
-    res.status(200).json(data);
-  } catch (error) {
-    console.error('Error fetching or parsing data:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+export default async function handler(request, response) {
+    const url = 'https://raw.githubusercontent.com/pwr406/finalproject/main/db.json?token=GHSAT0AAAAAACI65HSHP2TNWK72QAPCB5BMZKJXTAA';
+  
+    try {
+      const fetchData = await fetch(url);
+      const jsonData = await fetchData.json(); // Convert the fetched data to JSON
+  
+      response.status(200).json(jsonData); // Sending the JSON data as a response
+    } catch (error) {
+      console.error('Error fetching or parsing data:', error);
+      response.status(500).json({ error: 'Internal Server Error' });
+    }
   }
-}
