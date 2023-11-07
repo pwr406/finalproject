7b28f92a0e230fb814a9b92f82f5a72f6d27409d
Paul Ryan <paul.walker.ryan@gmail.com>
2023-11-06 21:50:39 -0700

    another serverless function change

--- api/getData.js
+++ api/getData.js
@@ -1,10 +1,19 @@
+import fetch from 'node-fetch'; // Ensure you've imported fetch or use any other HTTP library
+
 export default async function handler(req, res) {
-    const url = 'https://raw.githubusercontent.com/pwr406/finalproject/main/db.json?token=GHSAT0AAAAAACI65HSHP2TNWK72QAPCB5BMZKJXTAA'; 
-  
-    // Fetch the JSON data
+  const url = 'https://raw.githubusercontent.com/pwr406/finalproject/main/db.json?token=GHSAT0AAAAAACI65HSHP2TNWK72QAPCB5BMZKJXTAA';
+
+  try {
     const response = await fetch(url);
-    const data = await response.json();
-  
+    const rawData = await response.text(); // Fetch as text
+
+    // Manually parse the fetched text as JSON
+    const data = JSON.parse(rawData);
+
     // Return the data
     res.status(200).json(data);
+  } catch (error) {
+    console.error('Error fetching or parsing data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
+}
