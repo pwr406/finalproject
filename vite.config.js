8b9ae0a1568cfd0c0f7024182b422d19a753003c
Paul Ryan <paul.walker.ryan@gmail.com>
2023-11-11 16:25:33 -0700

    working on env variable

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {  
+  'process.env.VITE_APP_YOUR_KEY':JSON.stringify(process.env.VITE_APP_YOUR_KEY)
+  }
 })
